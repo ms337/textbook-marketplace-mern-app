46d1f18b801218cc42b4318f5abed441d82e7d5c
Madhav <msinghal@uwo.ca>
2019-12-25 17:04:17 +0530

    reverted back to original store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import thunk from "redux-thunk";
 
@@ -12,7 +11,7 @@ const middleware = [thunk]; //any middleware we use will be inside this array
 const store = createStore(
 	rootReducer,
 	initialState,
-	composeWithDevTools(
+	compose(
 		applyMiddleware(...middleware),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
 	)
