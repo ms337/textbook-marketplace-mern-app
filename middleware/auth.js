ea929f0e8056acf2a6e755afa024165faa69250a
Madhav <msinghal@uwo.ca>
2019-12-28 12:42:32 +0530

    Trying to see whats wrong when posting a book and token is not valid

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,22 +3,22 @@ const jwt = require("jsonwebtoken");
 
 //Middleware function
 function auth(req, res, next) {
-  const token = req.header("x-auth-token");
+	const token = req.header("x-auth-token");
 
-  //checking if token
-  if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
-  }
+	//checking if token
+	if (!token) {
+		return res.status(401).json({ message: "No token, authorization denied" });
+	}
 
-  try {
-    //Verify the token
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-    //Add user to request from paylod
-    req.user = decoded;
-    next();
-  } catch (e) {
-    res.status(400).json({ message: "Token is not valid." });
-  }
+	try {
+		//Verify the token
+		const decoded = jwt.verify(token, config.get("jwtSecret"));
+		//Add user to request from paylod
+		req.user = decoded;
+		next();
+	} catch (e) {
+		res.status(400).json({ message: "Token is not valid." });
+	}
 }
 
 module.exports = auth;
